90cf56487cada7f720972db7ed74c5431bd71c79
lynx1097 <abdelrahman1097@outlook.com>
2026-05-19 22:35:02 +0300

    Create encrypt_env.py

---
+++ encrypt_env.py
@@ -0,0 +1,19 @@
+from cryptography.fernet import Fernet
+import json
+
+secrets = {
+    "GEMINI_API_KEY":os.getenv("GEMINI_API_KEY"),
+    "GEMINI_API_KEY_2":os.getenv("GEMINI_API_KEY_2"),
+    "GEMINI_API_KEY_3":os.getenv("GEMINI_API_KEY_3"),
+    "NEW_RELIC_INSERT_KEY":os.getenv("NEW_RELIC_INSERT_KEY"),
+}
+
+key = Fernet.generate_key()
+f = Fernet(key)
+encrypted = f.encrypt(json.dumps(secrets).encode())
+
+# Save these as Python files to bundle
+with open("_key.bin", "wb") as kf:
+    kf.write(key)
+with open("_secrets.bin", "wb") as sf:
+    sf.write(encrypted)
